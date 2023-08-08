28b5d8c4c3a72fefd549cdf822f2da80abbd869a
LeeDLUffy <leemmugambi7@gmail.com>
2023-08-08 17:01:45 +0300

    Finalised

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "telephony";
+$dbname = "iptelephony";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
--- load_campuses.php
+++ load_campuses.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "telephony";
+$dbname = "iptelephony";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
